91830559ce08e3c8006bd969be7be597f76a7915
khuongnguyenn <khuongnguyen05032006@gmail.com>
2026-09-21 13:01:17 +0700

    bai1.1_tuan2

---
+++ tuan2/bai1.1/bai1.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace bai1_1;
+class SinhVien
+{
+    public string HoTen { get; set; }
+    public int NamSinh { get; set; }
+    public void NhapThongTin()
+    {
+        Console.Write("Nhap ho ten: ");
+        HoTen = Console.ReadLine();
+        Console.Write("Nhap nam sinh: ");
+        NamSinh = int.Parse(Console.ReadLine());
+    }
+    public int TinhTuoi()
+    {
+        int NamHienTai = DateTime.Now.Year;
+        int Tuoi = NamHienTai - NamSinh;
+        return Tuoi;
+    }
+    public void XuatThongTin()
+    {
+        Console.WriteLine("Tuoi sinh vien la: " + TinhTuoi());
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        SinhVien sv = new SinhVien();
+        sv.NhapThongTin();
+        sv.XuatThongTin();
+        Console.ReadKey();
+    }
+}
